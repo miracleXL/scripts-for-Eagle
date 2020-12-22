5f96b6805d1696e1321d4a33b29744859c3bf2b9
miracleXL <1250036141@qq.com>
2020-12-22 22:23:34 +0800

    一键三连

--- Save Pixiv pictures to Eagle.user.js
+++ Save Pixiv pictures to Eagle.user.js
@@ -2,7 +2,7 @@
 // @name         Save Pixiv pictures to Eagle
 // @name:zh-CN   下载Pixiv图片到Eagle
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  Collect pictures in pixiv to eagle.
 // @author       miracleXL
 // @match        https://www.pixiv.net/artworks/*
@@ -38,17 +38,23 @@
                 "sec-fetch-site": "cross-site",
             };
 
+    // 每秒尝试加载一次
+    let loadMain = setInterval(main, 1000);
+
     function main(){
+        let divs_section = document.getElementsByClassName("sc-181ts2x-0 jPZrYy")[0];
+        if(divs_section === undefined) return;
         //暂时无法保存动图，如果是动图则什么都不做
         if(document.getElementsByClassName("tu09d3-1 MNNrM").length !== 0){
             console.log("无法保存动图！");
             return;
         }
         let image = document.getElementsByClassName("sc-1qpw8k9-3 ckeRFU")[0];
-        // 暂未实现区分
         let mode = image ? "image" : "manga" ;
 
-        addButtons();
+        addButtons(mode,divs_section);
+        document.getElementsByClassName("sc-1yvhotl-2 hGipVQ")[0].onchange = main;
+        window.clearInterval(loadMain);
     }
 
     function download(data){
@@ -141,9 +147,43 @@
         })
     }
 
+    function getImageData(){
+        let images = document.getElementsByClassName("sc-1qpw8k9-3 ckeRFU")[0];     // 单图
+        images = images ? images : document.getElementsByClassName("sc-1qpw8k9-3 lmFZOm")[0];   //多图
+        //获取标题
+        let name = document.getElementsByClassName("sc-1u8nu73-3 feoVvS")[0];
+        if(name === undefined){
+            name = "";
+        }else{
+            name = name.textContent;
+        }
+        //获取描述
+        let annotation = document.getElementById("expandable-paragraph-0");
+        if(annotation){annotation = annotation.textContent;}
+        else{annotation = "";}
+        //把pixiv标签和标签翻译添加进eagle标签
+        let tags = [];
+        let firstTag = document.getElementsByClassName("nqp4a5-0")[0];
+        if(firstTag !== undefined){
+            tags.push(firstTag.textContent);
+        }
+        document.getElementsByClassName(TAG_CLASS).forEach(item => {tags.push(item.text);});
+        document.getElementsByClassName(TAG_TRANS_CLASS).forEach(item => {tags.push(item.text);});
+        let data = {
+            "url": images.href,
+            "name": name,
+            "website": document.URL,
+            "tags": tags,
+            "headers": HEADERS
+        }
+        let author = document.getElementsByClassName("sc-10gpz4q-5 bUnVlH")[0].textContent.split("@")[0];
+        return [data,author];
+    };
+
+
     function getImagesData(){
-        let images = document.getElementsByClassName("sc-1qpw8k9-3 ckeRFU");
-        images = images[0] ? images : document.getElementsByClassName("sc-1qpw8k9-3 lmFZOm");
+        let images = document.getElementsByClassName("sc-1qpw8k9-3 ckeRFU");    //单图
+        images = images[0] ? images : document.getElementsByClassName("sc-1qpw8k9-3 lmFZOm");   //多图
         let data = {"items":[]}
         //获取标题
         let name = document.getElementsByClassName("sc-1u8nu73-3 feoVvS")[0];
@@ -153,7 +193,9 @@
             name = name.textContent;
         }
         //获取描述
-        let annotation = document.getElementById("expandable-paragraph-0").textContent;
+        let annotation = document.getElementById("expandable-paragraph-0");
+        if(annotation){annotation = annotation.textContent;}
+        else{annotation = "";}
         //把pixiv标签和标签翻译添加进eagle标签
         let tags = [];
         let firstTag = document.getElementsByClassName("nqp4a5-0")[0];
@@ -185,17 +227,21 @@
     }
 
     //在收藏按钮旁边添加下载按钮
-    function addButtons(){
+    function addButtons(mode,buttonPos){
         let button = document.createElement('div');
         button.setAttribute('class', 'sc-181ts2x-01');
         button.setAttribute('style', 'margin-right: 23px;');
-        button.innerHTML = '<button type="button" id="download" class="_35vRH4a"><span class="_3uX7m3X">下载</span></button>';
-        let divs_section = document.getElementsByClassName("sc-181ts2x-0 jPZrYy")[0];
-        if(divs_section !== undefined){
-            divs_section.appendChild(button);
+        button.innerHTML = '<button type="button" id="download" class="_35vRH4a"><span class="_3uX7m3X">下载全部</span></button>';
+        if(buttonPos !== undefined){
+            buttonPos.appendChild(button);
         }
         //绑定点击按钮时下载事件
         button.addEventListener("click", async function(){
+            //下载同时自动点赞+收藏
+            try{
+                document.getElementsByClassName("_35vRH4a")[0].click();
+                document.getElementsByClassName("kgq5hw-0 iPGEIN gtm-main-bookmark")[0].click();
+            }catch(e){}
             let [data, author] = getImagesData();
             let dlFolderId = await getFolderId(author);
             if(dlFolderId === undefined){
@@ -207,7 +253,26 @@
             downloadAll(data);
             changeStyle(button);
         });
+        // 若为多图添加复选框和下载选择按键……计划中是这样的
+        if(mode === "manga"){
+            let button2 = document.createElement('div');
+            button2.setAttribute('class', 'sc-181ts2x-01');
+            button2.setAttribute('style', 'margin-right: 23px;');
+            button2.innerHTML = '<button type="button" id="download_1" class="_35vRH4a"><span class="_3uX7m3X">下载首张</span></button>';
+            if(buttonPos) buttonPos.appendChild(button2);
+            button2.addEventListener("click", async function(){
+                let [data, author] = getImageData();
+                let dlFolderId = await getFolderId(author);
+                if(dlFolderId === undefined){
+                    console.log("创建文件夹失败！尝试直接下载……")
+                }
+                else{
+                    data.folderId = dlFolderId;
+                }
+                download(data);
+                changeStyle(button);
+            })
+        }
     };
 
-    setTimeout(main, 1000);
 })();
